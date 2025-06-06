11248f8a4f7f7de0ff655227e6d06f6b13d1fd9c
Calvin <calvin.curnuck@gmail.com>
2025-06-06 10:52:02 -0400

    comment run in server.py to prevent double server run in main.py

--- server/server.py
+++ server/server.py
@@ -70,5 +70,5 @@ def delete_cached_data():
         )
 
 
-if __name__ == '__main__':
-    uvicorn.run(app, host='0.0.0.0', port=8998)
+# if __name__ == '__main__':
+#     uvicorn.run(app, host='0.0.0.0', port=8998)
